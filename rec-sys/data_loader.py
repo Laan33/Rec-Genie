b5763bc70e5b1b89eeb0df0bc9222bcf2aefda1b
Cathal Lawlor <cathal.lawlor@outlook.com>
2025-03-18 21:23:14 +0000

    I'm moving the ml-32m ratings to films/. Credits from movies/7/ to films/. same with movies_metadata

--- rec-sys/data_loader.py
+++ rec-sys/data_loader.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 
 
-dataset_path = os.path.join(os.path.expanduser('~'), 'datasets', 'movies')
+dataset_path = os.path.join(os.path.expanduser('~'), 'datasets', 'films')
 
 def mount_drive():
     from google.colab import drive
@@ -13,10 +13,10 @@ def mount_drive():
 
 def load_movies():
     # TODO - need to drop movies_df = movies_df.drop(35587) # A weird film entry is now gone!
-    return pd.read_csv(dataset_path + "movies_metadata.csv", usecols=['id', 'title', 'release_date', 'genres', 'popularity', 'vote_average', 'vote_count'])
+    return pd.read_csv(dataset_path + "/movies_metadata.csv", usecols=['id', 'title', 'release_date', 'genres', 'popularity', 'vote_average', 'vote_count'])
 
 def load_ratings():
-    return pd.read_csv(dataset_path + "ratings.csv", usecols=['userId', 'movieId', 'rating'])
+    return pd.read_csv(dataset_path + "/ratings.csv", usecols=['userId', 'movieId', 'rating'])
 
 def load_credits():
-    return pd.read_csv(dataset_path + "credits.csv")
+    return pd.read_csv(dataset_path + "/credits.csv")
--- rec-sys/main.py
+++ rec-sys/main.py
@@ -1,5 +1,9 @@
+import pandas as pd
+
 import data_loader
-import user_profile
+import hybrid_recommender
+import preprocessing as pre
+import user_profile as user_pf
 
 # Change this to True if you are running on Google Colab
 RUNNING_ON_COLAB = False
@@ -12,8 +16,36 @@ USER_ID = 999,999
 if RUNNING_ON_COLAB:
     data_loader.mount_drive()
 
+# Load the data
+films_df = data_loader.load_movies()
+ratings_df = data_loader.load_ratings()
+credits_df = data_loader.load_credits()
 
-user_profile
+# Data preprocessing
+# Drop the weird film entry
+films_df = films_df.drop(35587)
+
+# One-hot encode the genres
+ohe_films_df = pre.one_hot_encode_genres(films_df)
+
+# Process the credits metadata
+credits_df = pre.condense_credits(credits_df)
+
+# Tidy the noise and merge the credits metadata with the films DataFrame
+films_df = pre.data_tidying(ohe_films_df, credits_df)
+
+# Generate the user profile
+user_profile_df = user_pf.load_user_profile()
+user_profile = user_pf.create_user_profile(USER_ID, films_df, ratings_df, user_profile_df)
+
+# Append the user profile to the ratings DataFrame
+ratings_df = pd.concat([ratings_df, user_profile_df], ignore_index=True)
+
+
+# Generate recommendations
+recommendations = hybrid_recommender.hybrid_recommend(USER_ID, user_profile, films_df, credits_df, ratings_df)
+hybrid_recommender.score_breakdown(films_df, recommendations)
+
 
 
 
