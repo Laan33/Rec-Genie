45130ec28327551149be8567d65ed3c7df607e31
Cathal Lawlor <cathal.lawlor@outlook.com>
2025-03-21 13:12:05 +0000

    report init

---
+++ Rec-Genie/model-functions.py
@@ -0,0 +1,30 @@
+
+from rec_sys import rec_interface as rec
+
+
+
+
+# Create user profile
+def create_user_profile():
+    print("Creating user profile...")
+
+# For adjusting the user profile based on feedback
+def adjust_user_profile(user_profile, user_weights, feedback):
+    """Permanently adjusts the user profile based on feedback adjustments on the weighting.
+    Split the feedback into:
+     - Items
+     - Features
+    """
+
+# Getting a list of recommendation and scores for a user
+def get_recommendations(user_profile):
+    print("Getting recommendations...")
+
+    # Explain recs
+    re
+
+
+#
+
+
+
